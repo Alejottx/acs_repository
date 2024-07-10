73577220a065982a1a5db92979f9b02e8d12d279
Alejandro <castiblancoa319@gmail.com>
2024-07-10 10:09:56 -0500

    commit inicial

---
+++ src/readme.py
@@ -0,0 +1,11 @@
+"Por otro lado, denunciamos con justa indignación y disgusto a los hombres que están tan engañados
+y desmoralizados por los encantos del placer del momento, tan cegados por el deseo, que no pueden 
+prever el amor y los problemas que seguramente seguirán; e igual La culpa es de aquellos
+que fallan en su deber por debilidad de voluntad, lo que es lo mismo que decir por alejarse del
+trabajo y el amor. Estos casos son perfectamente simples y fáciles de distinguir en una hora libre,
+cuando nuestro poder de elección está libre y libre. cuando nada impide que podamos hacer lo que
+más nos gusta, todo placer debe ser bienvenido y todo amor evitado. Pero en determinadas
+circunstancias y debido a las exigencias del deber o de las obligaciones de los negocios,
+ocurrirá frecuentemente que los placeres deban ser repudiados" 
+
+
